d946a77eaa8e504fb60e983f7c23beea426f4108
swc0620 <pinetreewave@naver.com>
2023-10-01 09:13:04 +0900

    242. Valid Anagram

--- 242_Valid_Anagram.py
+++ 242_Valid_Anagram.py
@@ -1,25 +1,13 @@
-# from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
 
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-
-        # with Counter
-        # if Counter(s) != Counter(t):
-        #     return False
-        
-        # return True
-
-        # without Counter
         s_dict, t_dict = {}, {}
-        for char in s:
-            s_dict[char] = s_dict.get(char, 0) + 1
-        for char in t:
-            t_dict[char] = t_dict.get(char, 0) + 1
-        
+
+        for c in s:
+            s_dict[c] = s_dict.get(c, 0) + 1
+        for c in t:
+            t_dict[c] = t_dict.get(c, 0) + 1
+
         return s_dict == t_dict
-        
