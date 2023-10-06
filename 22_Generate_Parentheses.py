1099976f7583b1c0e74dadd579a713f75c418066
swc0620 <pinetreewave@naver.com>
2023-10-06 16:40:28 +0900

    22. Generate Parentheses

---
+++ 22_Generate_Parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+
+        def backtracking(op, cl, string, stack):
+            if op < 0 or cl < 0:
+                return
+            if op == 0 and cl == 0:
+                ans.append(string)
+
+            backtracking(op-1, cl, string+"(", stack+"(")
+            if stack and stack[-1] == "(":
+                backtracking(op, cl-1, string+")", stack[:-1])
+        
+        backtracking(n, n, "", "")
+        return ans
