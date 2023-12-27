f90d7ba957d5702c4df96b9f7251bf778c1ae648
swc0620 <pinetreewave@naver.com>
2023-12-27 22:50:03 +0900

    100. Same Tree

---
+++ 100_Same_Tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        def helper(node1, node2):
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+            
+            left = helper(node1.left, node2.left)
+            right = helper(node1.right, node2.right)
+
+            return left and right
+        
+        return helper(p, q)
