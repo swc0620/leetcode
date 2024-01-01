b02996a77cb541cdfe9d9628784a14168a3ced4f
swc0620 <pinetreewave@naver.com>
2024-01-01 13:45:55 +0900

    199. Binary Tree Right Side View

---
+++ 199_Binary_Tree_Right_Side_View.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def digTree(node, level):
+            if not node:
+                return
+            
+            nonlocal res
+            if len(res) <= level:
+                res.append(node.val)
+            else:
+                res[level] = node.val
+
+            digTree(node.left, level+1)
+            digTree(node.right, level+1)
+
+            return
+        
+        digTree(root, 0)
+        return res
