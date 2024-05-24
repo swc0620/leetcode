f4cc0d2502789c221899d6a0f8df26a304601ee8
swc0620 <pinetreewave@naver.com>
2024-05-25 02:15:49 +0900

    200. Number of Islands

--- 200_Number_of_Islands.py
+++ 200_Number_of_Islands.py
@@ -2,26 +2,26 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def bfs(start, grid):
-            deq = deque()
-            deq.append(start)
-            while deq:
-                row, col = deq.popleft()
-                grid[row][col] = "0"
-                if row + 1 < len(grid) and grid[row+1][col] == "1" and (row+1, col) not in deq:
-                    deq.append((row+1, col))
-                if col + 1 < len(grid[0]) and grid[row][col+1] == "1" and (row, col+1) not in deq:
-                    deq.append((row, col+1))
-                if row - 1 >= 0 and grid[row-1][col] == "1" and (row-1, col) not in deq:
-                    deq.append((row-1, col))
-                if col - 1 >= 0 and grid[row][col-1] == "1" and (row, col-1) not in deq:
-                    deq.append((row, col-1))
-    
-        cnt = 0
-        for row_idx, row in enumerate(grid):
-            for col_idx, col in enumerate(row):
-                if col == "1":
-                    cnt += 1
-                    bfs((row_idx, col_idx), grid)
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        res = 0
+
+        def bfs(r, c):
+            queue = deque()
+            directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+            queue.append((r, c))
+            while queue:
+                r, c = queue.pop()
+                for dx, dy in directions:
+                    x, y = r + dx, c + dy
+                    if x in range(rows) and y in range(cols) and grid[x][y] == "1" and (x, y) not in visited:
+                        queue.append((x, y))
+                        visited.add((x, y))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    res += 1
         
-        return cnt
+        return res
