f1af8b4dd639f4ef3458e0f3fbf96183f6b8b52d
swc0620 <pinetreewave@naver.com>
2024-05-27 02:03:11 +0900

    125. Valid Palindrome

--- 125_Valid_Palindrome.py
+++ 125_Valid_Palindrome.py
@@ -1,6 +1,20 @@
-import re
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        stack = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
-        return stack == stack[::-1]
+        def isalnum(c):
+            c = ord(c)
+            return (ord('a') <= c <= ord('z')) or (ord('A') <= c <= ord('Z')) or (ord('0') <= c <= ord('9'))
+
+        pre, post = 0, len(s) - 1
+        while True:
+            while pre < len(s)-1 and not isalnum(s[pre]):
+                pre += 1
+            while post > 0 and not isalnum(s[post]):
+                post -= 1
+            if pre >= post:
+                return True
+        
+            if s[pre].lower() != s[post].lower():
+                return False
+            
+            pre += 1
+            post -= 1
