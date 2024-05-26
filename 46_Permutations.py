e446792607841efd3e686107f83d900b810944a7
swc0620 <pinetreewave@naver.com>
2024-05-27 03:05:53 +0900

    46. Permutations

---
+++ 46_Permutations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        for _ in range(len(nums)):
+            num = nums.pop(0)
+            perms = self.permute(nums)
+            for perm in perms:
+                perm.append(num)
+            res.extend(perms)
+            nums.append(num)
+
+        return res
