c767df627a20baa5349e0d73b446bc904e09b565
swc0620 <pinetreewave@naver.com>
2024-05-28 02:42:39 +0900

    121. Best Time to Buy and Sell Stock

--- 121_Best_Time_To_Buy_And_Sell_Stock.py
+++ 121_Best_Time_To_Buy_And_Sell_Stock.py
@@ -3,11 +3,11 @@ class Solution:
         min_price, max_price = prices[0], prices[0]
         res = 0
         for price in prices:
-            max_price = max(max_price, price)
-            res = max(res, max_price - min_price)
             if price < min_price:
-                res = max(res, max_price - min_price)
                 min_price = price
                 max_price = price
+                continue
+            max_price = max(price, max_price)
+            res = max(res, max_price - min_price)
         
         return res
