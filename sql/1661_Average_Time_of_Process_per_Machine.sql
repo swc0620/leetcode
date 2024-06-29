a3fb2cf8778b8270c58ff63e7b8921596947b196
swc0620 <pinetreewave@naver.com>
2024-06-30 03:01:30 +0900

    1661. Average Time of Process per Machine

---
+++ sql/1661_Average_Time_of_Process_per_Machine.sql
@@ -0,0 +1,3 @@
+SELECT machine_id, ROUND(SUM(CASE WHEN activity_type='start' THEN timestamp * -1 ELSE timestamp END) / (SELECT COUNT(DISTINCT process_id)), 3) AS processing_time
+FROM Activity
+GROUP BY machine_id;
