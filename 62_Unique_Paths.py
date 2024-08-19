d33f3186345dfae712c0790c89a75a37a4ed03a9
swc0620 <pinetreewave@naver.com>
2024-08-19 06:22:13 -0400

    62. Unique Paths

---
+++ 62_Unique_Paths.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for i in range(m-2, -1, -1):
+            new_row = [1] * n
+            for j in range(n-2, -1, -1):
+                new_row[j] = row[j] + new_row[j+1]
+            row = new_row
+        
+        return row[0]
