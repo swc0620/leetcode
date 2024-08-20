10d1cc56adf12357086b19efbd6eb0c242792883
swc0620 <pinetreewave@naver.com>
2024-08-20 00:41:27 -0400

    1046. Last Stone Weight

--- 1046_Last_Stone_Weight.py
+++ 1046_Last_Stone_Weight.py
@@ -2,17 +2,14 @@ import heapq
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        for i in range(len(stones)):
-            stones[i] *= -1
-        heapq.heapify(stones)
-
-        while len(stones) > 1:
-            y = heapq.heappop(stones) * -1
-            x = heapq.heappop(stones) * -1
-            if y-x > 0:
-                heapq.heappush(stones, x-y)
+        heap = []
+        for stone in stones:
+            heapq.heappush(heap, -stone)
         
-        if stones:
-            return stones[0] * -1
-        return 0
+        while len(heap) > 1:
+            stone1 = heapq.heappop(heap)
+            stone2 = heapq.heappop(heap)
+            heapq.heappush(heap, abs(stone1 - stone2) * -1)
+        
+        return heap[0] * -1
         
