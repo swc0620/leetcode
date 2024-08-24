856ce8a247641e549f86f960f7919602cdaf1c43
swc0620 <pinetreewave@naver.com>
2024-08-24 14:10:16 -0400

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- 105_Constuct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
+++ 105_Constuct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -6,24 +6,11 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        inorder_map = {}
-        for idx, val in enumerate(inorder):
-            inorder_map[val] = idx
-        root_idx = 0
-
-        def digTree(left, right):
-            if left > right:
-                return None
-            
-            nonlocal root_idx
-
-            root_val = preorder[root_idx]
-            root = TreeNode(root_val)
-            root_idx += 1
-
-            root.left = digTree(left, inorder_map[root_val] - 1)
-            root.right = digTree(inorder_map[root_val] + 1, right)
-
-            return root
-
-        return digTree(0, len(preorder) - 1)
+        if not preorder or not inorder:
+            return None
+        
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        return root
