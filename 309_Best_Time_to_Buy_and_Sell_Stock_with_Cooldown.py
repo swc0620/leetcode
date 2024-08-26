2827078f9e433d7dae770d2adc5fd19a77192e24
swc0620 <pinetreewave@naver.com>
2024-08-26 12:48:47 -0400

    309. Best Time to Buy and Sell Stock with Cooldown

--- 309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.py
+++ 309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.py
@@ -2,19 +2,20 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         dp = {}
 
-        def dfs(idx, is_turn_to_buy):
-            if idx >= len(prices):
+        def dfs(i, turn_to_buy):
+            if i >= len(prices):
                 return 0
-            if (idx, is_turn_to_buy) in dp:
-                return dp[(idx, is_turn_to_buy)]
+            if (i, turn_to_buy) in dp:
+                return dp[(i, turn_to_buy)]
             
-            cooldown = dfs(idx+1, is_turn_to_buy)
-            if is_turn_to_buy:
-                buy = dfs(idx+1, not is_turn_to_buy) - prices[idx]
-                dp[(idx, is_turn_to_buy)] = max(buy, cooldown)
+            cooldown = dfs(i+1, turn_to_buy)
+            if turn_to_buy:
+                buy = dfs(i+1, not turn_to_buy) - prices[i]
+                dp[(i, turn_to_buy)] = max(cooldown, buy)
             else:
-                sell = dfs(idx+2, not is_turn_to_buy) + prices[idx]
-                dp[(idx, is_turn_to_buy)] = max(sell, cooldown)
-            return dp[(idx, is_turn_to_buy)]
+                sell = dfs(i+2, not turn_to_buy) + prices[i]
+                dp[(i, turn_to_buy)] = max(cooldown, sell)
+            return dp[(i, turn_to_buy)]
         
         return dfs(0, True)
+            
