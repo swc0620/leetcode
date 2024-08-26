7d3bbdff4af46c6a091ad53620407eeaaa8d2e57
swc0620 <pinetreewave@naver.com>
2024-08-26 03:36:34 -0400

    322. Coin Change

--- 322_Coin_Change.py
+++ 322_Coin_Change.py
@@ -1,16 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount+1] * (amount+1)
+        dp = [(amount+1)] * (amount+1)
         dp[0] = 0
-
-        for c in coins:
-            if c < amount:
-                dp[c] = 1
-
-        for n in range(1, amount+1):
-            for c in coins:
-                if n - c >= 0:
-                    dp[n] = min(dp[n], dp[n-c] + 1)
+        
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i-coin]+1, dp[i])
         
         if dp[amount] == (amount+1):
             return -1
