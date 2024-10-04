09e67792ffc64856651ccd196b54d2692adb677e
swc0620 <pinetreewave@naver.com>
2024-10-04 19:29:02 -0400

    2. Add Two Numbers

--- 2_Add_Two_Numbers.py
+++ 2_Add_Two_Numbers.py
@@ -8,31 +8,19 @@ class Solution:
         head = dummy = ListNode()
         carry = 0
         while l1 and l2:
-            if carry == 1:
-                q, r = divmod(l1.val + l2.val + carry, 10)
-            else:
-                q, r = divmod(l1.val + l2.val, 10)
+            carry, r = divmod(l1.val + l2.val + carry, 10)
             head.next = ListNode(r, None)
             head = head.next
-            carry = q
             l1, l2 = l1.next, l2.next
         while l1:
-            if carry == 1:
-                q, r = divmod(l1.val + carry, 10)
-            else:
-                q, r = divmod(l1.val, 10)
+            carry, r = divmod(l1.val + carry, 10)
             head.next = ListNode(r, None)
             head = head.next
-            carry = q
             l1 = l1.next
         while l2:
-            if carry == 1:
-                q, r = divmod(l2.val + carry, 10)
-            else:
-                q, r = divmod(l2.val, 10)
+            carry, r = divmod(l2.val + carry, 10)
             head.next = ListNode(r, None)
             head = head.next
-            carry = q
             l2 = l2.next
         if carry:
             head.next = ListNode(carry, None)
