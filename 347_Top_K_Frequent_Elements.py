34fa6214b81f47e348c741143823207081465264
swc0620 <pinetreewave@naver.com>
2024-10-06 23:29:29 -0400

    347. Top K Frequent Elements

--- 347_Top_K_Frequent_Elements.py
+++ 347_Top_K_Frequent_Elements.py
@@ -1,16 +1,19 @@
+from collections import Counter
+import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
         for num in nums:
-            count[num] = count.get(num, 0) + 1
-
-        freq = [[] for _ in range(len(nums) + 1)]
-        for n, c in count.items():
-            freq[c].append(n)
-
+            if num not in count:
+                count[num] = 1
+            else:
+                count[num] += 1
+        
+        heap = [(-1 * value, key) for key, value in count.items()]
+        heapq.heapify(heap)
         res = []
-        for i in range(len(freq)-1, -1, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        for _ in range(k):
+            _, key = heapq.heappop(heap)
+            res.append(key)
+        return res
