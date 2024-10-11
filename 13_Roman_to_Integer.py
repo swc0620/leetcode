4169abcea5b7f32eee4665b02176602dae533c4b
swc0620 <pinetreewave@naver.com>
2024-10-11 00:20:30 -0400

    13. Roman to Integer

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -1,15 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        r2i = {'O': 0, 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        s = s + 'O'
+        r2i = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         res = 0
-        for i in range(len(s)-1):
-            curr = s[i]
-            _next = s[i+1]
-            if r2i[curr] < r2i[_next]:
-                res -= r2i[curr]
+        for i in range(len(s)):
+            if i+1 < len(s) and r2i[s[i]] < r2i[s[i+1]]:
+                res -= r2i[s[i]]
             else:
-                res += r2i[curr]
+                res += r2i[s[i]]
         return res
 
         
